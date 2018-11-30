8610b0006b0ee043b001f325e60a6d162e189246
parmezan38 <luka38.stojanac@gmail.com>
2018-11-30 14:52:46 +0100

    removed temporary users from lobby data rooms

--- bin/data/lobby_data.js
+++ bin/data/lobby_data.js
@@ -1,10 +1,16 @@
 let lobby = {
     rooms: [
         {   name: "Room 1",     maxUsers: 4,    users: [],   id: "room001"  },
-        {   name: "Room 2",     maxUsers: 4,    users: ["A","B","C"],   id: "room002"  },
-        {   name: "Room 3",     maxUsers: 4,    users: ["A","B","C", "D"],   id: "room003"  },
-        {   name: "Room 4",     maxUsers: 4,    users: ["A","B"],   id: "room004"  },
-        {   name: "Room 5",     maxUsers: 4,    users: ["A","B","C","D"],   id: "room005"  }
+        {   name: "Room 2",     maxUsers: 4,    users: [],   id: "room002"  },
+        {   name: "Room 3",     maxUsers: 4,    users: [],   id: "room003"  },
+        {   name: "Room 4",     maxUsers: 4,    users: [],   id: "room004"  },
+        {   name: "Room 5",     maxUsers: 4,    users: [],   id: "room005"  },
+        {   name: "Room 6",     maxUsers: 4,    users: [],   id: "room006"  },
+        {   name: "Room 7",     maxUsers: 4,    users: [],   id: "room007"  },
+        {   name: "Room 8",     maxUsers: 4,    users: [],   id: "room008"  },
+        {   name: "Room 9",     maxUsers: 4,    users: [],   id: "room009"  },
+        {   name: "Room 10",    maxUsers: 4,    users: [],   id: "room010"  }
+
     ]
 };
 module.exports = lobby;
